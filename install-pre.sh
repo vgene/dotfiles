076a0c077aeee75fc2d37ca2ead8056b1f93422b
Ziyang Xu <ziyangx@princeton.edu>
2020-10-12 12:30:29 -0400

    update pre

--- install-pre.sh
+++ install-pre.sh
@@ -2,6 +2,8 @@
 git clone https://github.com/joelthelion/autojump.git
 cd autojump
 ./install.py
+cd ..
+rm -rf autojump
 
 # Install Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
